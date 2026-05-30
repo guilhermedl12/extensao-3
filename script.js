818aa9531a7ebae17321315d1b5be19432b858c3
guilhermedl12 <guilhermedl_12@outlook.com>
2026-05-29 21:22:53 -0300

    att chart.js

--- script.js
+++ script.js
@@ -5,25 +5,19 @@ const ctx = document.getElementById('meuGrafico').getContext('2d');
 const meuGrafico = new Chart(ctx, {
     type: 'doughnut', // Define o tipo de gráfico: 'bar', 'line', 'pie', 'doughnut', etc.
     data: {
-        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'], // Legendas do eixo X
+        labels: ['Adolescentes', 'Adultos', 'Idosos'], // Legendas do eixo X
         datasets: [{
-            label: 'Número de Vendas',
-            data: [12, 19, 3, 5, 2, 3], // Os dados que serão plotados
+            label: 'Vítimas',
+            data: [12, 19, 3], // Os dados que serão plotados
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
         }]
